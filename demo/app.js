3b600e81464905202af468c2901e7f32143880b3
rojer <rojerchen@qq.com>
2022-02-12 12:51:44 +0800

    fix: :bug: 导入方式

--- demo/app.js
+++ demo/app.js
@@ -1,12 +1,12 @@
 // app.js
-const katex = require('@rojer/katex-mini')
+import { loadKatex } from '@rojer/katex-mini'
 App({
   onLaunch() {
-
+    // 通过动态方式加载katex代码，节省小程序包体大小
     wx.request({
       url: 'https://lib.baomitu.com/KaTeX/latest/katex.min.js',
       success: ({ data: code })=>{
-        katex.loadKatex(code);
+        loadKatex(code);
         wx.katex = true
       }
     })
